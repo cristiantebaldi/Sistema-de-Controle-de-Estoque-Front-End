78d1005a83d7682a9473a41595a1beed395442e6
Cristian Luís Tebaldi <158230439+cristiantebaldi@users.noreply.github.com>
2024-11-17 23:09:51 -0300

    :construction: página de relatório e estoque

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,44 @@
 <template>
     <div class="d-flex mt-5">
-          <v-card 
-          class="d-flex align-center"
-          rounded="e-lg"
-          height="50" 
-          >
-          <v-card-text>
-            FILTRAR POR:
-          </v-card-text>
-          <v-card-actions>
-              <v-btn
-              width="120"
-              variant="tonal"
-              color="red"
-              >Vendas</v-btn>
-              <v-btn
-              width="120"
-              variant="tonal"
-              color="green"
-              >Compras</v-btn>
-            </v-card-actions>
-          </v-card>
+      <v-card 
+      class="d-flex align-center"
+      rounded="e-lg"
+      height="50" 
+      >
+      <v-card-text>
+        FILTRAR POR:
+      </v-card-text>
+      <v-card-actions>
+          <v-btn
+          width="120"
+          variant="tonal"
+          color="red"
+          >Vendas</v-btn>
+          <v-btn
+          width="120"
+          variant="tonal"
+          color="green"
+          >Compras</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+        
+        <div>
+          <v-container>
+            <v-data-table
+              :headers="headersSale"
+              :items="sales"
+              :height="500"
+              >
+              <template #item.total_arrecadado="{ item }">
+                {{ formatMoney(item.total_arrecadado) }}
+              </template>
+              <template #item.data_venda="{ item }">
+                {{ formatDate(item.data_venda) }}
+              </template>
+            </v-data-table>
+          </v-container>
         </div>
-  
-        <v-container>
-          <v-data-table
-          :items="sales"
-          :height="380">
-          </v-data-table>
-        </v-container>
   </template>
   
   <script setup>
@@ -37,6 +47,31 @@
   import { ref, onMounted } from "vue";
   
   const sales = ref([]);
+
+  const headersSale = ref([
+    {title: "Produto", value: "produto"},
+    {title: "Data", value: "data_venda"},
+    {title: "Quantidade", value: "quantidade_vendida"},
+    {title: "Valor Total", value: "total_arrecadado"},
+  ]);
+  
+  const formatMoney = (value) => {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(value);
+  };
+
+  const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("pt-BR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+  };
 
   onMounted(() => {
     fetchSales();
--- src/views/StockControl.vue
+++ src/views/StockControl.vue
@@ -1,10 +1,86 @@
 <template>
+     <div class="d-flex mt-5">
+      <v-card 
+        class="d-flex align-center"
+        rounded="e-lg"
+        height="50" 
+        >
+        <v-card-text>
+          NOVO(A):
+        </v-card-text>
+      </v-card>
+      
+      <v-card-actions>
+        <v-btn
+        width="120"
+        variant="tonal"
+        color="green"
+        prepend-icon="mdi-tag-plus"
+        >Venda</v-btn>
+        <v-btn
+        width="120"
+        variant="tonal"
+        color="yellow"
+        prepend-icon="mdi-package-variant-closed-check"
+        >Repor</v-btn>
+        <v-btn
+        width="120"
+        variant="tonal"
+        color="orange"
+        prepend-icon="mdi-package-variant-closed-plus"
+        >Produto</v-btn>
+        <v-btn
+        width="200"
+        variant="tonal"
+        color="deep-orange-darken-2"
+        prepend-icon="mdi-exclamation"
+        >Baixo Estoque</v-btn>
+      </v-card-actions>
+    </div>
+
     <div>
-      <h1>Controle de Estoque</h1>
-      <p>Bem-vindo à página de controle de estoque.</p>
+      <v-container>
+        <v-data-table
+          :headers="headersProduct"
+          :items="products"
+          :height="500">
+          <template #item.preco="{ item }">
+            {{ formatMoney(item.preco) }}
+          </template>
+        </v-data-table>
+      </v-container>
     </div>
 </template>
-  <script>
-  
+  <script setup>
+    import axios from "axios";
+    import { ref, onMounted } from "vue";
+
+    const products = ref([]);
+
+    const headersProduct = ref([
+    {title: "Produto", value: "nome"},
+    {title: "Estoque Atual", value: "quantidade"},
+    {title: "Preço unitário", value: "preco"},
+    {title: "Estoque Mínimo", value: "quantidade_min"},
+  ]);
+
+    const formatMoney = (value) => {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(value);
+  };
+
+    onMounted(() => {
+      fetchProducts();
+    })
+
+    const fetchProducts = async () => {
+    const response = await axios.get(`http://localhost:8080/produto`)
+    products.value = response.data
+  };
+
   </script>
   
